d2933ce3b3986eb78d5120787c270c10a77dd20c
Qixuan "Harrison" Ma <maq@mssm.org>
2021-11-14 23:31:32 -0800

    finished

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Lab 8 - Starter
+
+1. Within a Github action that runs whenever code is pushed. This allows us to be very efficient with testing via automation while making sure that we are not pushing non-functional code to GitHub repo. 
+2. No. 
+3. No. This would be part of the UI test. This feature would include 2 parts: sending the message and receiving the message. We cannot check both ends with a unit test.
+4. Yes. We can. For instance, we can paste a longer passage (80+ characters) and check if that message is allowed to be sent.
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -3,3 +3,88 @@
 const functions = require('../code-to-unit-test/unit-test-me.js');
 
 // TODO - Part 2
+
+// Unit test for isEmail 
+test('school email is email', () => {
+    expect(functions['isEmail']('q5ma@ucsd.edu')).toBe(true)
+});
+
+test('personal email is email', () => {
+    expect(functions['isEmail']('qixuanma2001@gmail.com')).toBe(true)
+});
+
+test('name is not email', () => {
+    expect(functions['isEmail']('Harrison Ma')).toBe(false)
+});
+
+test('phone number is not email', () => {
+    expect(functions['isEmail']('100-100-1000')).toBe(false)
+});
+
+// Unit test for isPhoneNumber
+test('email is not phone number', () => {
+    expect(functions['isPhoneNumber']('q5ma@ucsd.edu')).toBe(false)
+});
+
+test('date is not phone number', () => {
+    expect(functions['isPhoneNumber']('2000-01-01')).toBe(false)
+});
+
+test('phone number is phone number', () => {
+    expect(functions['isPhoneNumber']('100-100-1000')).toBe(true)
+});
+
+test('international phone number is phone number', () => {
+    expect(functions['isPhoneNumber']('189-7919-1000')).toBe(true)
+});
+
+// Unit test for isStrongPassword
+test('password is not string password', () => {
+    expect(functions['isStrongPassword']('123')).toBe(false)
+});
+
+test('simple password is not strong password', () => {
+    expect(functions['isStrongPassword']('12345678')).toBe(false)
+});
+
+test('random string is strong password', () => {
+    expect(functions['isStrongPassword']('woshishui876')).toBe(true)
+});
+
+test('another random string is strong password', () => {
+    expect(functions['isStrongPassword']('nihao123')).toBe(true)
+});
+
+// Unit test for isDate
+test('phone number is not date', () => {
+    expect(functions['isDate']('189-7919-1000')).toBe(false)
+});
+
+test('name is not date', () => {
+    expect(functions['isDate']('Harrison Ma')).toBe(false)
+});
+
+test('date is date', () => {
+    expect(functions['isDate']('11/21/1968')).toBe(true)
+});
+
+test('another date is date', () => {
+    expect(functions['isDate']('10/01/1949')).toBe(true)
+});
+
+// Unit test for isHexColor
+test('phone number is not HexColor', () => {
+    expect(functions['isHexColor']('189-7919-1000')).toBe(false)
+});
+
+test('name is not HexColor', () => {
+    expect(functions['isHexColor']('Harrison Ma')).toBe(false)
+});
+
+test('3-digit HexColor is HexColor', () => {
+    expect(functions['isHexColor']('FFF')).toBe(true)
+});
+
+test('6-digit HexColor is HexColor', () => {
+    expect(functions['isHexColor']('FAFAFA')).toBe(true)
+});
